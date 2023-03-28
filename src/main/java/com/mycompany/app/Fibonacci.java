600d91adc9f88fef97ce5616793f7a9f89d4400a
Guilherme Amaral <gui.amaral123@outlook.com>
2023-03-28 10:57:14 +0000

    Fibanacci

--- src/main/java/com/mycompany/app/Fibonacci.java
+++ src/main/java/com/mycompany/app/Fibonacci.java
@@ -2,14 +2,19 @@ package com.mycompany.app;
 
 import java.util.Scanner;
 
+/*
+ * 
+ */
 public class Fibonacci {
 
     public static void main(String[] args) {
-        int a = 0, b = 1, c;
+        
         Scanner scan = new Scanner(System.in);
-        //TODO: terminar o input amanha
+        int a = 0, b = 1, c;
+        
+        System.out.println("Digite um número que você quer: ");
         int n = scan.nextInt();
-            for (int i = 0; a < n; i++) {
+        for (int i = 0; a < n; i++) {
         
             System.out.println(a);
             c = a + b;
