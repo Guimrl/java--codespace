8f3727ea0897fa92a3d7071325001a6d8d8c68de
Guilherme Amaral <gui.amaral123@outlook.com>
2023-04-02 19:41:30 +0000

    final commit

--- src/main/java/com/mycompany/app/BreakAString.java
+++ src/main/java/com/mycompany/app/BreakAString.java
@@ -1,10 +1,16 @@
 package com.mycompany.app;
+import java.util.Scanner;
 
 public class BreakAString {
-    
+
     public static void main(String[] args) {
-        String txt = "Esta string será quebrada Teste";
-        int tamanho = 5;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Insira uma string como argumento. ");
+        String txt = scan.nextLine();
+        scan.close();
+        
+        int tamanho = 10;
         int total = (int) Math.ceil((double) txt.length() / tamanho);
 
         for (int i = 0; i < total; i++) {
